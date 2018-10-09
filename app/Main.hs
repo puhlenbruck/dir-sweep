6b3096b23dc5e23378d8aa1d69ea2f9c28e9a6de
Peter Uhlenbruck <peuhlenb@gmail.com>
2018-10-09 00:49:37 -0700

    add option parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,48 @@
 module Main where
 
+import Options.Applicative
+import Data.Semigroup ((<>))
 import System.Directory
 import Data.Time.Clock
+import Data.List (sortOn)
+import Data.Ord
+import Text.Read
+import Data.Maybe
 
 main :: IO ()
 main = do
-  files <- listFiles "/home/peter/Downloads"
-  mapM_ (print) files
+  opts <- execParser programOpts
+  sequence_ $ fmap (runForDir opts) (dirs opts)
 
-data FileTime = FileTime {name :: FilePath, modifyTime :: UTCTime}
+data Options = Options 
+  { dirs :: [FilePath]
+  , maxKeepCount :: Maybe Int
+  }
+
+programOpts = info (options <**> helper)
+  (fullDesc
+  <> progDesc "Clean old files from a directory"
+  <> header "cleanup - a temp directory cleaning tool")
+
+options :: Parser Options
+options = Options
+  <$> some ( argument str (metavar "TARGET..." <> help "directories to clean" ))
+  <*> option optionalInt (long "max-keep" <> short 'n' <> help "the maximum number of files to keep" <> value Nothing <> metavar "MAX-KEEP")
+
+optionalInt :: ReadM (Maybe Int)
+optionalInt = eitherReader $ \s -> maybe (Left $ "Cannot parse max-keep value '" ++ s ++ "'. Must be an integer.") (Right . Just) (readMaybe s)
+
+runForDir :: Options -> FilePath -> IO ()
+runForDir opts dir = do
+  files <- sortOn (Down . modifyTime) <$> listFiles dir
+  let filesToRemove = maybeDrop (maxKeepCount opts) files
+  mapM_ print filesToRemove
+
+maybeDrop :: Maybe Int -> [a] -> [a]
+maybeDrop (Just n) list = drop n list
+maybeDrop Nothing _ = []
+
+data FileAndModTime = FileAndModTime {name :: FilePath, modifyTime :: UTCTime}
   deriving (Show)
 
 thresholdTime :: DiffTime -> IO UTCTime
@@ -17,14 +51,13 @@ thresholdTime timePeriod = do
   let nominalTimePeriod = realToFrac timePeriod
   return $ addUTCTime nominalTimePeriod currentTime
 
-
-listFiles :: FilePath -> IO [FileTime]
+listFiles :: FilePath -> IO [FileAndModTime]
 listFiles dir = do
   files <- listDirectory dir
   let fullPaths = fmap (\name -> dir ++ "/" ++ name) files
-  mapM (fileWithModTime) fullPaths
+  mapM fileWithModTime fullPaths
 
-fileWithModTime :: FilePath -> IO FileTime
+fileWithModTime :: FilePath -> IO FileAndModTime
 fileWithModTime file = do
   modTime <- getModificationTime file
-  return $ FileTime file modTime
+  return $ FileAndModTime file modTime
